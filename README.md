207bab3b18e741b892c1e52f4e4d504d0c33df8e
mudittiwari <mudit19.072@gmail.com>
2024-12-25 11:05:00 +0530

    vite config change

--- README.md
+++ README.md
@@ -3,3 +3,9 @@ conditions:
 player has zero tokens in the playing area
 player has 1 token in the playing area
 player has more than 1 token in the playing area
+
+
+work to do:
+implement collision logic in the game
+implement 3 player in the game
+implement the 4 player in the game
--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,10 @@ import legacy from '@vitejs/plugin-legacy';
 
 const getConfig = ({ command, mode }) => ({
   server: {
-    host: '0.0.0.0',
     hmr: {
-      clientPort: 3000,
-    }
+      protocol: 'ws', // or 'wss' for HTTPS
+      host: 'localhost',
+    },
   },
   plugins: [
     react(),
